4a64dfe4904af09a486638918a7e28d4c8929eac
myo900 <myoweb900@gmail.com>
2025-10-29 21:08:28 +0100

    Delete src directory

--- src/battery_config.cpp
+++
@@ -1,5 +0,0 @@
-// battery_config.cpp - IMPLEMENTAZIONE COMPLETA CON AUTO-APPRENDIMENTO
-#include "battery_config.h"
-
-// Istanza globale della configurazione batteria
-BatteryConfig batteryConfig;
